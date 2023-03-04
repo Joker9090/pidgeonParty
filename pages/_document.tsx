b13beb917e68d747a947404c1cc7b5114232316b
Barto <juanmartin.cerruti@gmail.com>
2023-03-04 11:27:00 -0300

    v0

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="stylesheet" as="script" href='/globals.css' />
       <body>
         <Main />
         <NextScript />
